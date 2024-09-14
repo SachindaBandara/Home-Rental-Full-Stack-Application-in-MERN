320fe1b9b95ab3e48248ae06e4314dde01c7966f
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-14 23:44:15 +0530

    add home and contact files

--- vehicle-marketplace/src/main.jsx
+++ vehicle-marketplace/src/main.jsx
@@ -1,15 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Contact } from "./contact"
+import { Home } from "./home"
 
 
-const router = createBrowserRouter{{
+const router = createBrowserRouter([
+   {
+    path:'/',
+    element:<Home/>
+   },
 
-}}
+   {
+    path:'/contact',
+    element:<Contact/>
+   },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
-  </StrictMode>,
+  <RouterProvider router={router}/>
+  </StrictMode>
 )

3fa2d469b0abd5ca5dbdbec4f4408c246b548d63
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-14 23:29:37 +0530

    create home jsx file

--- vehicle-marketplace/src/main.jsx
+++ vehicle-marketplace/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter } from 'react-router-dom'
 
@@ -11,6 +10,6 @@ const router = createBrowserRouter{{
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  
   </StrictMode>,
 )

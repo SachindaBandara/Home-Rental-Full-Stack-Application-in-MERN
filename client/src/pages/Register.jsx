cc6d0b7c731343d1276407de5d2e638698ca80b1
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-19 22:55:22 +0530

    create register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="register">
+      <div className="register_content">
+        <form action="">
+          <input
+            type="text"
+            placeholder="First Name"
+            name="firstname"
+            required
+          />
+          <input 
+            type="text" 
+            placeholder="Last Name" 
+            name="lastname" 
+            required 
+          />
+          <input 
+            type="email" 
+            placeholder="Email" 
+            name="email" 
+            required 
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmpassword"
+            required
+          />
 
-export default Register
+          <input 
+            type="file" 
+            name="profileimage" 
+            accept="image/*" 
+            style={{display:'none'}}
+            required />
+        </form>
+        <p>
+          Already have an account? <a href="/login">Login here</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;

e5acd5b07d45bcad03aa1af75874c6798a882096
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-20 22:34:43 +0530

    add registration contents

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,52 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/Register.scss'
 
 const Register = () => {
+
+  const [formData, setFormData ] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+    profileImage:null
+  })
+
+  const handleChange = (e) =>{
+    const { name, value, files } = e.target
+    setFormData({
+      ...formData,
+      [name]: value,
+      [name]: name === "profileImage" ? files[0]: value
+    })
+  }
+
+
   return (
     <div className="register">
       <div className="register_content">
-        <form action="">
+        <form action="" className="register_content_form">
           <input
             type="text"
             placeholder="First Name"
-            name="firstname"
+            name="firstName"
+            value={formData.firstName}
             required
           />
-          <input type="text" placeholder="Last Name" name="lastname" required />
-          <input type="email" placeholder="Email" name="email" required />
+
+          <input 
+            type="text" 
+            placeholder="Last Name" 
+            name="lastName" 
+            value={formData.lastName}
+            required 
+          />
+
+          <input 
+            type="email" 
+            placeholder="Email" 
+            name="email" 
+            value={formData.email}
+            required 
+          />
           <input
             type="password"
             placeholder="Password"
             name="password"
+            value={formData.password}
             required
           />
           <input
             type="password"
             placeholder="Confirm Password"
-            name="confirmpassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
             required
           />
 
           <input
             id="image"
             type="file"
-            name="profileimage"
+            name="profileImage"
             accept="image/*"
             style={{ display: "none" }}
+            value={formData.profileImage}
             required
           />
 
           <label htmlFor="image">
-            <img src="/assets/addImage" alt="Add Profile Photo" />
+            <img src="../../public/assets/addImage.png" alt="Add Profile Photo" />
             <p>Upload Your Photo</p>
           </label>
 
           <button type="submit">Register</button>
 
         </form>
-        <p>
+        <a>
           Already have an account? <a href="/login">Login here</a>
-        </p>
+        </a>
       </div>
     </div>
   );

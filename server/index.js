11b426de13025a1fbd4500fd18cd1a5bf4666b35
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-21 09:39:21 +0530

    connected  monogoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
-const express = require("express")
-const app= express()
-const mongoose = require("mongoose")
-const dotenv = require("dotenv").config()
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static("public"))
+// mongoose setup
+
+const PORT = 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));

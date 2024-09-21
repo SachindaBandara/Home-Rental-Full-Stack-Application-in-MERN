eb87be02358f04ca5bf91e0640e09698401d84ed
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-21 21:10:41 +0530

    create login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import '../styles/Login.scss'
 
 const Login = () => {
-  return (
-    <div>
-      <p>hi</p>
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  return (
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form">
+          <input 
+            type="email" 
+            placeholder="Email"
+            value={email} 
+            onChange={ (e)=> e.target.value} 
+            required 
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={ (e)=> e.target.value}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+        <a href="/register">Don't have an account? Sign In here</a>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

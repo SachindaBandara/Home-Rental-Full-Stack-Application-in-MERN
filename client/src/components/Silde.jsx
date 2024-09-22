c66c6369ca05f94da0ddc421e64fe87b2246ba92
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-22 14:00:12 +0530

    Create silde in the home page

---
+++ client/src/components/Silde.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Slide.scss"
+
+const Silde = () => {
+  return (
+    <div className='slide'>
+        <h1>
+            Welcome Home! Anywhere you room <br/> Stay in the moment. Make your memories
+        </h1>
+    </div>
+  )
+}
+
+export default Silde
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Slide from "../components/Silde";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Slide/>
     </>
   );
 };

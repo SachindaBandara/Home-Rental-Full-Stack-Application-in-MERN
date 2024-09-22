ea044562e01182365b30106dc2f6daf73f20861d
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-22 15:25:08 +0530

    add

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Slide from "../components/Silde";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Slide/>
+      <Categories/>
+      
     </>
   );
 };

f97121abad60253e79dc3d8bf050a6197a4a5d81
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-22 16:21:18 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,11 +1,124 @@
-import React from 'react'
+import React from "react";
+import "../styles/CreateListing.scss";
+import Navbar from "../components/Navbar";
+import { categories, types } from "../data";
+import { RemoveCircleOutline, AddCircleOutline, AddCircleRounded } from "@mui/icons-material";
+import variables from "../styles/variables.scss";
 
 const CreateListing = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Navbar />
 
-export default CreateListing
+      <div className="create-listing">
+        <h1>Publish Your place</h1>
+        <form>
+          <div className="create-listing-step1">
+            <h2>Step:1 Tell us about your place</h2>
+            <hr />
+            <h3>Which of these categories best describes your place?</h3>
+            <div className="category-list">
+              {categories?.map((item, index) => (
+                <div className="category" key={index}>
+                  <div className="category_icon">{item.icon}</div>
+                  <p>{item.label}</p>
+                </div>
+              ))}
+            </div>
+
+            <h3>What type of place will guests have?</h3>
+            <div className="type-list">
+              {types?.map((item, index) => (
+                <div className="type">
+                  <div className="type_text">
+                    <h4>{item.name}</h4>
+                    <p>{item.description}</p>
+                  </div>
+                  <div className="type_icon">{item.icon}</div>
+                </div>
+              ))}
+            </div>
+
+            <h3>Where's your place located?</h3>
+            <div className="full">
+              <div className="location">
+                <p>Street Address</p>
+                <input
+                  type="text"
+                  placeholder="Street Address"
+                  name="streetAddress"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="half">
+              <div className="location">
+                <p>Apartment, Suite, ect.(if applicable)</p>
+                <input
+                  type="text"
+                  placeholder="Apartment, Suite, ect.(if applicable)"
+                  name="aptSuite"
+                  required
+                />
+              </div>
+
+              <div className="location">
+                <p>City</p>
+                <input type="text" placeholder="City" name="city" required />
+              </div>
+            </div>
+
+            <div className="half">
+              <div className="location">
+                <p>Province</p>
+                <input
+                  type="text"
+                  placeholder="Province"
+                  name="province"
+                  required
+                />
+              </div>
+
+              <div className="location">
+                <p>Country</p>
+                <input
+                  type="text"
+                  placeholder="Country"
+                  name="country"
+                  required
+                />
+              </div>
+            </div>
+
+            <h3>Share some basics about your place</h3>
+            <div className="basics">
+              <div className="basic">
+                <p>Guest</p>
+                <div className="basic_count">
+                  <RemoveCircleOutline
+                    sx={{
+                      fontSize: "25px",
+                      cursor: "pointer",
+                      "&:hover": { color: variables.pinkred },
+                    }}
+                  />
+                  <p>1</p>
+                  <AddCircleOutline
+                  sx={{
+                    fontSize: "25px",
+                    cursor: "pointer",
+                    "&:hover": { color: variables.pinkred },
+                  }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default CreateListing;
